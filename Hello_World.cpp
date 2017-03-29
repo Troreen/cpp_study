06a9f1ea4131c85e27029a64907cb2405ff1f49a
Troreen <ATT53.53.53@gmail.com>
2017-03-29 23:11:05 +0200

    Lesson 2 done!

--- Hello_World.cpp
+++ Hello_World.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
-int main () {
-	for (int i = 0; i < 10; ++i){
-		printf("Hello World!\n");
-	}
+using namespace std;
+
+int main () 
+{
+	int a = 4;
+	int b = 21;
+
+	int sum = 48 -7;
+
+	cout << sum;
+	
+	return 0;	
 }
+
